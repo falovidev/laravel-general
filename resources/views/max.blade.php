a8d362d4e5563e0d388f460b8a272b6c5b8eb700
Fabian Lozano <fabian.lozano@uac.edu.co>
2024-05-16 17:37:32 -0500

    preview basic 100%

--- resources/views/max.blade.php
+++ resources/views/max.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
 
 
 </head>
+
 <body>
 
     <div class="header">
@@ -16,19 +18,43 @@
             <div>Inicio</div>
             <div>Series</div>
             <div>Películas</div>
-            <div>loh_hbo</div>
+            <div class="hbo_logo"><img src="{{ asset('img/hbo_logo.webp') }}" alt=""></div>
             <div>Niños y Familia</div>
         </div>
         <div class="menu_right">
-        <div class="search"><img src="{{ asset('img/search.webp') }}" alt=""></div>
-        <div class="favorite"><img src="{{ asset('img/favorite.webp') }}" alt=""></div>
-        <div class="avatar"><img src="{{ asset('img/avatar.webp') }}" alt=""></div>
+            <div class="search"><img src="{{ asset('img/search.webp') }}" alt=""></div>
+            <div class="favorite"><img src="{{ asset('img/favorite.webp') }}" alt=""></div>
+            <div class="avatar"><img src="{{ asset('img/avatar.webp') }}" alt=""></div>
         </div>
     </div>
 
-    <div class="preview">
+    <div class="container_preview">
+        <div class="preview">
+
+            <div class="imge_title">
+                <div class="img_title"><img src="{{ asset('img/acuaman_title.webp') }}" alt=""></div>
+            </div>
+
+            <div class="metaData">
+                <div class="clasification">13+</div>
+                <div class="year">2023</div>
+            </div>
+            <div class="description">
+            Jason Momoa encarna nuevamente al superhéroe Aquaman, quien debe unir fuerzas con su hermano Orm para salvar a su familia de un poderoso enemigo.
+            </div>
+
+            <div class="button">
+                <button class="goto">Ir a la pelicula</button>
+            </div>
+
+        </div>
 
     </div>
-    
+    <div class="userList">
+
+    </div>
+
+
 </body>
+
 </html>
